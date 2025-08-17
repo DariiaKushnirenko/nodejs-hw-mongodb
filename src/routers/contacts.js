8a3f307c31035d938b92cbb71ae6ab24029f67ae
Dariia K <da23v12@gmail.com>
2025-08-17 15:04:44 +0100

    adding routes & controllers

---
+++ src/routers/contacts.js
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import {
+    getAllContactsController,
+    getContactByIdController,
+    createContactController,
+    deleteContactController,
+    patchContactController,
+} from '../controllers/contacts.js';
+
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
+const router = Router();
+
+router.get('/contacts', ctrlWrapper(getAllContactsController));
+
+router.get('/contacts/:contactId', ctrlWrapper (getContactByIdController));
+
+router.post('/contacts', ctrlWrapper(createContactController));
+
+router.patch('/students/:studentId', ctrlWrapper(patchContactController));
+
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import pinoHttp from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -13,44 +15,20 @@ export const setupServer = () => {
     app.use(cors());
 
     app.use(
-    pinoHttp({
-      transport: {
-        target: 'pino-pretty',
-        options: { colorize: true },
-      },
-    })
-  );
+        pinoHttp({
+            transport: {
+                target: 'pino-pretty',
+                options: { colorize: true },
+            },
+        }),
+    );
+    app.use('/contacts', contactsRouter);
 
-    app.get('/contacts', async (req, res) => {
-        const contacts = await getAllContacts();
-        res.status(200).json({
-            message: "Successfully found contacts!",
-            data: contacts,
-        });
-    });
+    app.use('/', notFoundHandler);
 
-    app.get('/contacts/:contactId', async (req, res, next) => {
-        const { contactId } = req.params;
-        const contact = await getContactById(contactId);
-
-        if (!contact) {
-            res.status(404).json({
-                message: 'Contact not found'
-            });
-            return;
-        }
-        res.status(200).json({
-            message: `Successfully found contact with id ${contactId}!`,
-            data: contact,
-        });
-    });
-   app.use((req, res) => {
-    res.status(404).json({ message: 'Not found' });
-});
+    app.use(errorHandler);
 
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
 };
-
-
